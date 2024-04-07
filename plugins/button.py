e212af4df5de79fa82bd702fca269740d723959b
XTellyFaris <164978067+XTellyFaris@users.noreply.github.com>
2024-04-07 19:36:45 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -8,9 +8,11 @@ from pyrogram.types import InlineKeyboardButton
 
 def start_button(client):
     if not FORCE_SUB:
-        buttons = dynamic_buttons + 
-         [
-            [InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close"),],
+        buttons = [
+            [
+                InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
+                InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close"),
+            ],
         ]
         return buttons
 
@@ -18,6 +20,11 @@ def start_button(client):
     num_force_sub = len(FORCE_SUB)
 
     current_row = []
+    for key in FORCE_SUB.keys():
+        current_row.append(InlineKeyboardButton(text=f"{BUTTONS_JOIN_TEXT} {key}", url=getattr(client, f'invitelink{key}')))
+        if len(current_row) == BUTTONS_PER_ROW:
+            dynamic_buttons.append(current_row)
+            current_row = []
 
     if current_row:
         dynamic_buttons.append(current_row)
